b8fe0bfa06cad76476e79c249b7e8388c1c45bd2
chalpenny <cp.halpenny@gmail.com>
2020-05-21 23:18:38 -0700

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+var titleCased = tutorials.map(title => {
+  title.toLowerCase().split(' ').map(function(word){
+    console.log(word.charAt(0).toUpperCase() + word.slice(1))
+}).join(' ')
+})
+ 
